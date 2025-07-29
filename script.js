ced7fc32ba41fe02b522be8be194ed84c79e1780
JoshuaMartinez210 <joshuamertinez@gmail.com>
2025-07-29 19:18:42 +0000

    Added JavaSc ript componets

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("hello world");
+
+let chicken = 123;
+console.log(chicken);
+console.log("chicken);
+
+consle.log(10 + "eggs");
+console.log(10 + 8 "eggs");
+consle.log("eggs"+ 10 +8);
+
+console.log(7!=4);
+
+console.log(7>3)&&(10.>8) 
+
+//Only one condition is true
+console.log(10>3) || (5>10);
+
+//If the statement is  logical or not
+console.log(!5>3);
+
+
+const hour =new Date().getHours
+let greeting = "";
+
+if (hour < 12) {
+  greeting = "Good morning!";
+}else if (hour < 18) {
+  greeting = "Good afternoon!";
+} else {
+    greeting = "Good evening!";
+    }
+
+document.getElementById("greeting").innerText = greeting;
